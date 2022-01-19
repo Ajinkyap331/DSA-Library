0cca2f219bbba3d6af11c86d4720653b25c91d2b
Pranav Masekar <83063593+PranavMasekar@users.noreply.github.com>
2022-01-19 15:29:12 +0530

    queue added

--- lib_queue.c
+++ lib_queue.c
@@ -1,10 +1,58 @@
 #include <stdio.h>
 
-// Queue using linked list
-void print_lsa(int val)
+struct Node *f = NULL;
+struct Node *r = NULL;
+struct Node
 {
-
-// Enqueue
-// Dequeue
-//     printf("My Fav Number is %d", val);
+    int data;
+    struct Node *next;
+};
+void display(struct Node *ptr)
+{
+    printf("Elements in the queue \n");
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }
+}
+void enqueue()
+{
+    int val;
+    printf("Enter the value \n");
+    scanf("%d", &val);
+    struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+    if (n == NULL)
+    {
+        printf("The queue is full");
+    }
+    else
+    {
+        n->data = val;
+        n->next = NULL;
+        if (f == NULL)
+        {
+            f = r = n;
+        }
+        else
+        {
+            r->next = n;
+            r = n;
+        }
+    }
+}
+void dequeue()
+{
+    int val;
+    struct Node *ptr = f;
+    if (f == NULL)
+    {
+        printf("The queue is Empty");
+    }
+    else
+    {
+        f = f->next;
+        val = ptr->data;
+        free(ptr);
+    }
 }
